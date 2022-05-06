e7b8f784ed8b77c289c2cc2ad349cd294da4f295
Martin Robinson <martinjrobins@gmail.com>
2022-05-06 17:46:41 +0100

    move include dir to right place

--- ports/casadi/portfile.cmake
+++ ports/casadi/portfile.cmake
@@ -18,11 +18,13 @@ vcpkg_fixup_cmake_targets(
 )
 
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
-file(RENAME "${CURRENT_PACKAGES_DIR}/casadi/pkgconfig/casadi.pc" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/casadi.pc")
-file(RENAME "${CURRENT_PACKAGES_DIR}/debug/casadi/pkgconfig/casadi.pc" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/casadi.pc")
+cfile(RENAME "${CURRENT_PACKAGES_DIR}/debug/casadi/pkgconfig/casadi.pc" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/casadi.pc")
 vcpkg_fixup_pkgconfig()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/casadi/pkgconfig")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/casadi/pkgconfig")
+
+file(RENAME "${CURRENT_PACKAGES_DIR}/casadi/include" "${CURRENT_PACKAGES_DIR}")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/casadi/include")
 
 file(
   INSTALL "${SOURCE_PATH}/LICENSE.txt"
